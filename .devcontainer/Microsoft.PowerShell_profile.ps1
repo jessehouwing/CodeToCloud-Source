01069fa73180a3cf05848c9b49c3294196b37c4f
Jesse Houwing <jesse.houwing@gmail.com>
2021-03-22 14:28:10 +0000

    find workspace

--- .devcontainer/Microsoft.PowerShell_profile.ps1
+++ .devcontainer/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,17 @@
-$workshopPath = Get-ChildItem -Path "/workspaces/" -Include ".workshop" -Recurse -Directory -Hidden | select-object -ExpandProperty "FullName" { $_.FullName }
+$workspacePaths = @("/home/codespace/workspace/", "/worspaces/")
+$workshopPath = ""
+
+foreach ($path in $workspacePaths){
+    if (Test-Path ){
+        $workshopPath = Get-ChildItem -Path $path -Include ".workshop" -Recurse -Directory -Hidden | select-object -ExpandProperty "FullName" { $_.FullName }
+        return;
+    }
+}
+
+if (-not $workshopPath){
+    Write-Error "Workspace folder not in the expected location."
+}
+
 $setupScript = join-path $workshopPath "setup.ps1"
 $workshopStepScript = join-path $workshopPath "workshop-step.ps1"
 
